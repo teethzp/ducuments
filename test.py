a3bf8dbf68f97e4cff473b6bdb1a241a59013ead
teethzp <2397955173@qq.com>
2017-11-02 11:26:50 +0800

    python sql test

---
+++ test.py
@@ -0,0 +1,9 @@
+#coding:utf-8
+import pymssql as mssql_module
+conn=mssql_module.connect(host='210.45.212.119',user='sa',password='admin@txsys2013',database='CPDB')
+#conn=mysql_module.connect(host='172.24.9.35',port=1433,user='sa',password='123456',database='CPDB')
+cur=conn.cursor()
+cur.execute('select * from [dbo].[USERINFO]')
+print(cur.fetchall())
+cur.close()
+conn.close()
