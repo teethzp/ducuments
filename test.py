7addef8e8fb2e7d60686396a2722b4f91ac8160c
teethzp <2397955173@qq.com>
2017-11-02 11:47:18 +0800

    test chinese

--- test.py
+++ test.py
@@ -4,6 +4,13 @@ conn=mssql_module.connect(host='210.45.212.119',user='sa',password='admin@txsys2
 #conn=mysql_module.connect(host='172.24.9.35',port=1433,user='sa',password='123456',database='CPDB')
 cur=conn.cursor()
 cur.execute('select * from [dbo].[USERINFO]')
-print(cur.fetchall())
+#print(cur.fetchall())
+r=cur.fetchone()
+i=1
+while r:
+	id,name,passwod,content=r
+	print(name)
+	r=cur.fetchone()
+	i+=1
 cur.close()
 conn.close()
